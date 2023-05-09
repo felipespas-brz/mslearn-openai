e6f8242fc9fd1bcd20582f8f6bd3cbb99f910e93
Felipe de Assis <felipe.assis@outlook.com>
2023-05-09 10:13:45 -0300

    completed exercise

--- Labfiles/02-nlp-azure-openai/Python/test-openai-model.py
+++ Labfiles/02-nlp-azure-openai/Python/test-openai-model.py
@@ -1,8 +1,12 @@
+## pre-requirements:
+## pip install python-dotenv
+## pip install openai
+
 import os
 from dotenv import load_dotenv
 
 # Add OpenAI import
-
+import openai
 
 def main(): 
         
@@ -15,10 +19,24 @@ def main():
         azure_oai_model = os.getenv("AZURE_OAI_MODEL")
         
         # Read text from file
-        text = open(file="../text-files/sample-text.txt", encoding="utf8").read()
+        # text = open(file="../text-files/sample-text.txt", encoding="utf8").read()
+        text = open(file="./Labfiles/02-nlp-azure-openai/text-files/sample-text.txt", encoding="utf8").read()
         
         # Set OpenAI configuration settings
-        
+        # Set OpenAI configuration settings
+        openai.api_type = "azure"
+        openai.api_base = azure_oai_endpoint
+        openai.api_version = "2022-12-01"
+        openai.api_key = azure_oai_key
+
+        # Send request to Azure OpenAI model
+        print("Sending request for summary to Azure OpenAI endpoint...\n\n")
+        response = openai.Completion.create(
+            engine=azure_oai_model,
+            prompt=text,
+            temperature=0.7,
+            max_tokens=60
+        )
         
 
         print("Summary of text: " + response.choices[0].text + "\n")
